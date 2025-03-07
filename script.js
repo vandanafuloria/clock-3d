43b23c165911b775726626e51aaa0b8c4b11c3dd
vandanafuloria <vandanafuloria02@gmail.com>
2025-03-07 17:30:30 +0530

    rotation hands are ready

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-const h1El = document.querySelector('h1');
-const h2El = document.querySelector('h2');
-const h3El = document.querySelector('h3');
-let clockEl = document.querySelector('.clock')
+const secondHandEl = document.querySelector('.second-hand');
+const minuteHandEl = document.querySelector('.minute-hand');
+const hourHandEl = document.querySelector('.hour-hand');
 
-function getTime(){
 
-    let date = new Date();
-    const format= date.toLocaleTimeString( 'en-IN' ,{hour12: true});
-    console.log(format);
-    h1El.innerText = format;
+function secondHandRotation() {
+    const sec = new Date();
+    let seconds = sec.getSeconds();
+     
+    seconds =  seconds * 6;
+    // const audio = new Audio("/assets/tik.mp3");
+    // audio.play();
+    // const audio = new Audio("/assets/tik.mp3"); // Absolute path
+    // audio.play();
+    //  audio.currentTime = 0;
+    //  audio.volume = 0.5;
+    secondHandEl.style.transform = `rotate(${seconds}deg)`;
+   
+    
+}
+ 
+function  minuteRotation(){
+    const min = new Date();
+    let minutes = min.getMinutes();
+    console.log(minutes);
+    minutes = minutes * 6 ;
+    console.log("working>>")
+    minuteHandEl.style.transform = `rotate(${minutes}deg)`;
 }
 
-
-function stopTime()
-{
-    clearInterval(handler);
+function hourRotaion(){
+    const hour = new Date();
+    let hours = hour.getHours();
+    hours = hours * 30 ;
+    hourHandEl.style.transform = `rotate(${hours}deg)`;
+ 
 }
 
-const handler = setInterval(getTime, 1000);
+ const handler1 = setInterval(secondHandRotation, 1000);
+const  handler2 = setInterval(minuteRotation, 60000)
+const handler3  = setInterval(hourRotaion, 1000 * 60 * 60 ); 
 
 
-// clockEl = addEventListener('mouseover', getTime)
-// clockEl = addEventListener('mouseout', stopTime)
-
-
-
-
+addEventListener("load",hourRotaion);
+addEventListener("load", minuteRotation)
+addEventListener('load', secondHandRotation);
